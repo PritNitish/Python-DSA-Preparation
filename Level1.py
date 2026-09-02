fb1c371265b48ed76971c0688726badc89dd5866
Nitish Kumar <nitishnitc@gmail.com>
2026-09-02 09:26:11 +0530

    Initial commit

---
+++ Level1.py
@@ -0,0 +1,396 @@
+def reverseString(str):
+    n= len(str)
+    result=""
+    for i in range(n):
+        result=str[i]+result
+
+    return result
+
+string1="nitish"
+check1=reverseString(string1)
+print(check1)
+
+def reverseInteger(num):
+
+    rem=0
+    sum=0
+    while(num!=0):
+        rem=num%10
+        sum=sum*10+rem
+        num=num//10
+    return sum
+
+num=567
+check2=reverseInteger(num)
+print(check2)
+
+def isPalindromeStr(str):
+    n= len(str)
+    for i in range(n//2):
+        if str[i]!=str[n-i-1]:
+            return False
+   
+    return True
+
+string= "hiih"
+check3=isPalindromeStr(string)
+print(check3)
+
+def isPalindromeNum(num):
+    temp=num
+    rem=0
+    sum=0
+    while(num!=0):
+
+        rem=num%10
+        sum=sum*10+rem
+        num=num//10
+
+    if(sum==temp):
+        return True
+    else:
+        return False
+
+num = 51151
+
+check4=isPalindromeNum(num)
+print(check4)
+
+def isAnagram(str1, str2):
+    if len(str1)!=len(str2):
+        return False
+
+    for i in range(len(str1)):
+        if str1.count(str1[i]) != str2.count(str1[i]):
+            return False
+    return True
+
+str1="listen"
+str2="silent"
+
+check5=isAnagram(str1,str2)
+print(check5)
+
+
+def firstNonRepeating(str):
+
+    for i in range(len(str)):
+        count=0
+        for j in range(len(str)):
+            if str[i]==str[j]:
+                count+=1
+        if count==1:
+            return str[i]
+
+    return None
+
+input="aaabbcde"
+check6=firstNonRepeating(input)
+print(check6)
+
+def charFreq(s):
+    for i in range(len(s)):
+        count = 0
+
+        for j in range(len(s)):
+            if s[i] == s[j]:
+                count += 1
+
+        print(s[i], "→", count)
+
+string = "hello"
+check7 = charFreq(string)
+print(check7)
+
+def countVowelConsonants(str):
+
+    n= len(str)
+    vowel_count=0
+    consonent_count=0
+    vowel="aeiou"
+    for i in range(n):
+        if str[i] in vowel:
+            vowel_count+=1
+        elif str[i].isalpha():
+            consonent_count+=1
+
+    return vowel_count, consonent_count
+string = "helloi"
+check8=countVowelConsonants(string)
+print(check8)
+
+def reverseWords(s):
+
+    result = ""
+    word = ""
+
+    for i in range(len(s)):
+        if s[i] != " ":
+            word = word + s[i]
+
+        else:
+            result = word + " " + result
+            word = ""
+
+    result = word + " " + result
+
+    return result
+
+
+sent = "I love Python"
+
+check10 = reverseWords(sent)
+print(check10)
+
+def removeDuplicate(arr):
+
+    result=[]
+    for i in range(len(arr)):
+        if arr[i] not in result:
+            result.append(arr[i])
+
+    return result
+
+arr=[1,2,2,3,1]
+check11=removeDuplicate(arr)
+print(check11)
+
+def findDuplicate(arr):
+
+    result=[]
+
+    for i in range(len(arr)):
+        count=1
+        for j in range(i+1,len(arr)):
+            if arr[i]==arr[j]:
+                count=count+1
+
+        if count>1:
+            result.append(arr[i])
+    return result
+
+arr = [1, 2, 3, 2, 4, 1, 5]
+check12=findDuplicate(arr)
+print(check12)
+
+def findSecondLargest(arr):
+    first=float("-inf")
+    sec=float("-inf")
+
+    for i in range(len(arr)):
+        if arr[i]>first:
+            sec=first
+            first=arr[i]
+
+        elif arr[i]>sec:
+            sec= arr[i]
+
+    return sec
+arr = [10, 5, 8, 20, 15]
+check13=findSecondLargest(arr)
+print(check13)
+
+def findSecondSmallest(arr):
+
+    first=float("inf")
+    sec=float("inf")
+
+    for i in range(len(arr)):
+        if arr[i]<first:
+            sec=first
+            first=arr[i]
+        elif arr[i]<sec:
+            sec=arr[i]
+
+    return sec
+arr = [10, 5, 8, 20, 15]
+check14=findSecondSmallest(arr)
+print(check14)
+    
+def secondMostFreq(arr):
+
+    result = []
+
+    for i in range(len(arr)):
+        count = 0
+
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1
+
+        if count not in result:
+            result.append(count)
+
+    first = float("-inf")
+    sec = float("-inf")
+
+    for i in range(len(result)):
+        if result[i] > first:
+            sec = first
+            first = result[i]
+
+        elif result[i] > sec:
+            sec = result[i]
+
+    for i in range(len(arr)):
+        count = 0
+
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1
+
+        if count == sec:
+            return arr[i]
+
+    return None
+
+
+arr = [1, 2, 2, 3, 3, 3, 4]
+
+check15 = secondMostFreq(arr)
+print(check15)
+
+
+def findMissingNum(arr):
+    n= len(arr)+1
+    sum=n*(n+1)//2
+    arr_sum=0
+
+    for i in arr:
+        arr_sum+=i
+
+    return sum-arr_sum
+
+arr = [1, 2, 4, 5, 6]
+check16=findMissingNum(arr)
+print(check16)
+
+def maxMin(arr):
+    max=float('-inf')
+    min=float("inf")
+
+    for i in range(len(arr)):
+        if arr[i]>max:
+            max=arr[i]
+
+        if arr[i]<min:
+            min=arr[i]
+
+    return max, min
+
+arr = [10, 5, 8, 20, 15]
+check17=maxMin(arr)
+print(check17)                                   
+
+def isArmstrong(num):
+
+    n= len(str(num))
+
+    rem=0
+    sum=0
+    temp=num
+    while(num!=0):
+        rem=num%10
+        sum=sum+rem**n
+        num=num//10
+
+
+    if(temp==sum):
+        return True
+    else:
+        return False
+
+num=153
+check18=isArmstrong(num)
+print(check18)
+
+def isPrime(num):
+
+    if num<2:
+        return False
+    count=0
+    for i in range(2,num//2+1):
+        if num%i==0:
+            count+=1
+
+    if count>0:
+        return False
+
+    return True
+
+num=17
+check19=isPrime(num)
+print(check19)
+
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        print(a, end=" ")
+        c = a + b
+        a = b
+        b = c
+
+    return None
+n = 7
+check20=fibonacci(n)
+print(check20)
+
+def fact(num):
+    fact=1
+    if num==0:
+        return 1
+    for i in range(1,num+1):
+        fact=fact*i
+
+    return fact
+check21=fact(6)
+print(check21)
+
+def gcd(num1, num2):
+
+    gcd = 1
+
+    for i in range(1, min(num1, num2) + 1):
+        if num1 % i == 0 and num2 % i == 0:
+            gcd = i
+
+    return gcd
+
+
+num1 = 12
+num2 = 18
+
+check22 = gcd(num1, num2)
+print(check22)
+
+def findLcm(num1, num2):
+
+    if num1 > num2:
+        start = num1
+    else:
+        start = num2
+
+    for i in range(start, num1 * num2 + 1):
+        if i % num1 == 0 and i % num2 == 0:
+            return i
+
+
+num1 = 4
+num2 = 6
+
+check23 = findLcm(num1, num2)
+print(check23)
+
+def sumDigit(num):
+    sum=0
+    while(num!=0):
+        rem =num%10
+        sum+=rem
+        num=num//10
+
+    return sum
+num=12345
+check24=sumDigit(num)
+print(check24)
---
+++ Level2.py
@@ -0,0 +1,395 @@
+def twoSum(arr,target):
+    n= len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]+arr[j] == target:
+                return i, j
+
+
+arr = [2, 7, 11, 15]
+target = 9
+Check1=twoSum(arr, target)
+print(Check1)
+
+
+def threeSum(arr,target):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            for k in range(j + 1, len(arr)):
+                if arr[i] + arr[j] + arr[k] == target:
+                    return i, j, k
+
+arr = [1, 2, 3, 4, 5]
+target=9
+
+Check2=threeSum(arr, target)
+print(Check2)
+
+def moveZero(arr):
+    n=len(arr)
+    result1=[]
+    result2=[]
+    for i in range(n):
+        if arr[i]==0:
+            result2.append(arr[i])
+        else:
+            result1.append(arr[i])
+    return result1+result2
+
+arr = [0, 1, 0, 3, 12]
+Check3=moveZero(arr)
+print(Check3)
+
+
+print("################################ Intersection ##################################")
+def interSection(arr1,arr2):
+    result=[]
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            if arr1[i]==arr2[j] and arr1[i] not in result:
+                result.append(arr1[i])
+    return result
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [3, 4, 5, 6, 7]
+Check4=interSection(arr1,arr2)
+print(Check4)
+
+def unionArray(arr1, arr2):
+    result=[]
+
+    for i in range(len(arr1)):
+        if arr1[i] not in result:
+            result.append(arr1[i])
+
+    for j in range(len(arr2)):
+        if arr2[j] not in result:
+            result.append(arr2[j])
+
+    return result
+
+
+arr1 = [1, 2, 2, 3, 4]
+arr2 = [2, 2, 3, 4, 5, 6]
+
+Check5=unionArray(arr1,arr2)
+print(Check5)
+
+def mergeTwoArray(arr1,arr2):
+
+    result=[]
+    i=0
+    j=0
+
+    while i<len(arr1) and j<len(arr2):
+        if arr1[i]<arr2[j]:
+            result.append(arr1[i])
+            i+=1
+        else:
+            result.append(arr2[j])
+            j+=1
+
+    while i<len(arr1):
+        result.append(arr1[i])
+        i+=1
+    while j<len(arr2):
+        result.append(arr2[j])
+        j+=1
+    return result
+arr1 = [0,1,3, 3, 5, 7]
+arr2 = [2, 4, 6, 8]
+
+Check6=mergeTwoArray(arr1,arr2)
+print(Check6)
+
+def commonElements(arr1, arr2):
+
+    result=[]
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            if arr1[i]==arr2[j] and arr1[i] not in result:
+                result.append(arr1[i])
+    return result
+
+arr1 = [1, 2, 2, 3, 4]
+arr2 = [2, 2, 4, 6]
+
+Check7=commonElements(arr1,arr2)
+print(Check7)
+
+
+def missingAndDuplicate(arr):
+
+    result=[]
+    n=len(arr)
+    desired_sum= n*(n+1)//2
+    arr_sum=0
+    result_sum=0
+    for i in range(n):
+        if arr[i] not in result:
+            result.append(arr[i])
+
+    for i in result:
+        result_sum+=i
+
+    for i in arr:
+        arr_sum+=i
+
+    missing= desired_sum-result_sum
+    duplicate= arr_sum-result_sum
+
+    return missing, duplicate
+
+arr = [1, 2, 2, 4, 5, 6]
+
+Check8= missingAndDuplicate(arr)
+print(Check8)
+
+def MajorityElement(arr):
+
+    element = None
+    count = 0
+
+    for i in range(len(arr)):
+
+        if count == 0:
+            element = arr[i]
+
+        if arr[i] == element:
+            count += 1
+        else:
+            count -= 1
+
+    # Verify majority element
+    count = 0
+
+    for i in range(len(arr)):
+        if arr[i] == element:
+            count += 1
+
+    if count > len(arr) // 2:
+        return element
+
+    return None
+
+
+arr = [2, 2, 1, 1, 1, 1, 2, 2, 2]
+
+Check9 = MajorityElement(arr)
+print(Check9)
+
+def maxSubArrSum(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+
+    for i in range(len(arr)):
+        current_sum += arr[i]
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
+            
+arr = [-2, 5, -1, 2, -3, 4, -10]
+Check10=maxSubArrSum(arr)
+print(Check10)
+
+def rotateRight(arr,k):
+
+    n=len(arr)
+    k=k%n
+    result=[]
+    for i in range(n-k, n):
+        result.append(arr[i])
+
+    for i in range(n-k):
+        result.append(arr[i])
+
+    return result
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+
+
+Check11=rotateRight(arr,k)
+print(Check11)
+
+def rotateLeft(arr,k):
+
+    n=len(arr)
+    k=k%n
+    result=[]
+    for i in range(k,n):
+        result.append(arr[i])
+
+    for i in range(k):
+        result.append(arr[i])
+
+    return result
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+Check12=rotateLeft(arr,k)
+print(Check12)
+
+def pairSum(arr, target):
+
+    n= len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+
+            sum=arr[i]+arr[j]
+
+            if sum==target:
+                return arr[i], arr[j]
+    
+arr = [2, 7, 11, 15]
+target = 9
+
+Check13=pairSum(arr, target)
+print(Check13)
+
+def pairDiff(arr, target):
+    n= len(arr)
+
+    for i in range(n):
+        for j in range(i+1,n):
+
+            diff = abs(arr[i]-arr[j])
+
+            if diff==target:
+                return arr[i], arr[j]
+
+arr = [5, 20, 3, 2, 50, 80]
+target = 78
+
+Check14=pairDiff(arr, target)
+print(Check14)
+
+def findLeaders(arr):
+    n= len(arr)
+    result=[]
+    leader=None
+    for i in range(n):
+        count=0
+        for j in range(i+1,n):
+
+            if arr[i]>arr[j]:
+                count+=1
+
+
+        if count==n-i-1:
+            result.append(arr[i])
+    return result
+
+arr = [16, 0,17,15, 4, 3, 5, 2]
+Check15=findLeaders(arr)
+print(Check15)
+
+def findEquilibrium(arr):
+    n= len(arr)
+    total_sum=0
+    left_sum=0
+    for i in range(n):
+        total_sum+=arr[i]
+
+    for j in range(n):
+        right_sum= total_sum-arr[j]-left_sum
+
+        if left_sum==right_sum:
+            return j
+        left_sum+=arr[j]
+
+    return -1
+
+arr = [1, 3, 5, 2, 1, 1]
+check16 = findEquilibrium(arr)
+print(check16)
+            
+
+def prodExceptSelf(arr):
+
+    result = []
+
+    for i in range(len(arr)):
+        product = 1
+
+        for j in range(len(arr)):
+            if i != j:
+                product = product * arr[j]
+
+        result.append(product)
+
+    return result
+
+
+arr = [1, 2, 3, 4]
+
+check17=prodExceptSelf(arr)
+print(check17)
+
+
+def prodExceptSelf(arr):
+
+    n = len(arr)
+    count = 0
+    result = []
+
+    # Count zeros
+    for i in range(n):
+        if arr[i] == 0:
+            count += 1
+
+    # More than one zero
+    if count > 1:
+        return [0] * n
+
+    # One zero
+    if count == 1:
+        total = 1
+
+        for i in range(n):
+            if arr[i] != 0:
+                total *= arr[i]
+
+        for i in range(n):
+            if arr[i] == 0:
+                result.append(total)
+            else:
+                result.append(0)
+
+        return result
+
+    # No zero
+    total = 1
+
+    for i in range(n):
+        total *= arr[i]
+
+    for i in range(n):
+        result.append(total // arr[i])
+
+    return result
+
+
+arr = [1, 2, 3, 4,0]
+
+print(prodExceptSelf(arr))
+   
+
+
+
+    
+
+
+
+
+        
+
+    
+
+        
